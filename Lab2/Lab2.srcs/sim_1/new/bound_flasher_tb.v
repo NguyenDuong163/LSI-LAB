d3d70356ce64d8ef992df8c33a3e19a889457319
LAPTOP-R91CRERV\84869 <duong.nguyenwt16@hcmut.edu.vn>
2024-03-31 10:25:40 +0700

    Re-update

--- Lab2/Lab2.srcs/sim_1/new/bound_flasher_tb.v
+++ Lab2/Lab2.srcs/sim_1/new/bound_flasher_tb.v
@@ -32,35 +32,33 @@ bound_flasher uut (reset_n,clock,flick,LED);
 
 initial 
 begin
-
     clock = 1;
     
     reset_n = 0;
     
     flick = 0;
     
-    #2
     
-    reset_n = 1;
     
     #2
     flick = 1;
+    reset_n = 1;
     
     #20
     
     flick = 0;
-    #600
+    #300
     
+    flick = 1;
+    
+    #200
     flick = 0;
-    
-    #500
-    
-    reset_n = 0;
+    //
     
     #1000
     $finish;
 end
 
-always #5 clock = ~ clock;
+always #2 clock = ~ clock;
    
 endmodule
